41e20d59ee4f696659483d7ac864ed4d9fde1aa2
Kevin Schaefer <kevinss.hello@gmail.com>
2021-10-21 09:54:31 -0700

    $set username removed from update user endpoint

--- index.js
+++ index.js
@@ -184,7 +184,6 @@ app.put(
       { Username: req.params.Username },
       {
         $set: {
-          Username: req.body.Username,
           Password: req.body.Password,
           Email: req.body.Email,
           Birthday: req.body.Birthday,
