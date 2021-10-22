23dc3ac939bb24f2e861be9e1de085634c6acc1e
Kevin Schaefer <kevinss.hello@gmail.com>
2021-10-22 11:53:19 -0700

    test: cors removed

--- index.js
+++ index.js
@@ -22,24 +22,24 @@ mongoose.connect("mongodb://localhost:27017/myFlixDB", {
 });
 // CORS added
 
-let allowedOrigins = ["http://localhost:8080", "https://git.heroku.com/my-flix-movies-app.git
-"];
-const cors = require("cors");
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        //if a specific origin isnt found on the list of allowed origins
-        let message =
-          "The CORS policy for this application doesn't allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// let allowedOrigins = ["http://localhost:8080", "https://git.heroku.com/my-flix-movies-app.git
+// "];
+// const cors = require("cors");
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         //if a specific origin isnt found on the list of allowed origins
+//         let message =
+//           "The CORS policy for this application doesn't allow access from origin " +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 app.use(morgan("common"));
 
